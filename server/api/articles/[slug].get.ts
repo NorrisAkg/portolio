f54efdc850db79c731c2d37340e8dd706c21ecad
NorrisAkg <norris.akogbede@epitech.eu>
2026-07-11 23:28:55 +0100

    fix(backend): resolve Nitro routing parameter conflict on [slug].get.ts

--- server/api/articles/[slug].get.ts
+++ server/api/articles/[slug].get.ts
@@ -4,7 +4,7 @@ import { handleDomainError } from '../../utils/handle-errors';
 import { useContainer } from '../../utils/use-container';
 
 const paramsSchema = z.object({
-  slug: z.string(),
+  id: z.string(), // Nitro maps the route parameter to 'id' because of the sibling [id]/ folder
 });
 
 export default defineEventHandler(async (event) => {
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event) => {
     const params = await getValidatedRouterParams(event, (data) => paramsSchema.parse(data));
     
     const useCase = useContainer().resolve<GetArticleUseCase>('GetArticleUseCase');
-    const article = await useCase.execute({ slug: params.slug });
+    const article = await useCase.execute({ slug: params.id });
 
     // Only admins can see DRAFT articles
     if (article.status === 'DRAFT') {
